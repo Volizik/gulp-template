e0b510babacdcc0507c44b56a9b39bbec18b043a
Kizilov <kizilov.yura@gmail.com>
2023-08-22 17:01:15 +0300

    updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,13 @@
-// TODO: install gulp-cli globally
+// have to install gulp-cli globally by command "npm i gulp-cli -g"
 
 const { dest, src, series, parallel, watch } = require('gulp');
-const sass = require("gulp-sass");
-const plumber = require('gulp-plumber');
-const sourcemaps = require('gulp-sourcemaps');
-const htmlExtender = require('gulp-html-extend');
-const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
+const htmlExtender = require('gulp-html-extend');
+const plumber = require('gulp-plumber');
 const browserSync = require('browser-sync').create();
-const del = require('del');
-const image = require('gulp-image');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify-es').default;
 
 function javascript() {
     return src('src/js/*.js')
@@ -31,23 +29,23 @@ function html() {
 
 function styles() {
     return (
-        src("./src/scss/styles.scss")
+        src('./src/scss/styles.scss')
             .pipe(plumber())
             .pipe(sourcemaps.init())
-            .pipe(sass({outputStyle: 'compressed'}))
             .pipe(autoprefixer())
-            .on("error", sass.logError)
+            .on('error', sass.logError)
             .pipe(sourcemaps.write())
             .pipe(plumber.stop())
-            .pipe(dest("./dist/css"))
+            .pipe(dest('./dist/css'))
             .pipe(browserSync.stream())
     );
 }
 
-function images() {
+async function images() {
+    const { default: imagemin } = await import('gulp-imagemin');
     return (
         src('./src/images/**/*')
-            .pipe(image())
+            .pipe(imagemin())
             .pipe(dest('./dist/images'))
             .pipe(browserSync.stream())
     )
@@ -56,20 +54,24 @@ function images() {
 function watchFiles() {
     browserSync.init({
         server: {
-            baseDir: "./dist",
+            baseDir: './dist',
             index: '/index.html'
         }
     });
 
-
     watch('./src/scss/**/*.scss', styles);
     watch('./src/html/**/*.html', html);
     watch('./src/js/**/*.js', javascript);
+    watch('./src/images/**/*', async (...params) => {
+        await clean('./dist/images')(...params)
+        await images(...params);
+    });
 }
 
-async function clean() {
-    await del(['./dist']);
+const clean = (path = './dist') => async () => {
+    const { deleteAsync } = await import('del');
+    await deleteAsync([path]);
 }
 
-exports.default = series(clean, parallel(styles, html, javascript), watchFiles);
-exports.build = series(clean, parallel(styles, html, javascript));
+exports.default = series(clean(), parallel(styles, html, javascript, images), watchFiles);
+exports.build = series(clean(), parallel(styles, html, javascript, images));
